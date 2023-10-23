a4786d2b79a5b494df4b517f0712a8d4e00dfed6
DarkSharpness_XzY <2040703891@qq.com>
2023-10-23 11:58:54 +0800

    Fix: Fix some wrong behaviors that may be slow.

--- src/int2048_impl.h
+++ src/int2048_impl.h
@@ -10,20 +10,23 @@ bool int2048::operator !(void) const noexcept { return this->is_non_zero(); }
 int2048_view int2048::operator + (void) & noexcept { return  int2048_view(*this); }
 int2048_view int2048::operator - (void) & noexcept { return -int2048_view(*this); }
 
-int2048 int2048::operator + (void) && noexcept { return  std::move(*this); }
+int2048 int2048::operator + (void) && noexcept { return std::move(*this); }
 int2048 int2048::operator - (void) && noexcept { return std::move(this->reverse()); }
 
 int2048 operator + (int2048_view lhs, int2048 &&rhs) { return std::move(rhs += lhs); }
 int2048 operator + (int2048 &&lhs, int2048_view rhs) { return std::move(lhs += rhs); }
 int2048 operator + (int2048 &&lhs, int2048 &&rhs) { return std::move(lhs += std::move(rhs)); }
 
-int2048 operator - (int2048_view lhs, int2048_view rhs) { return lhs + (-rhs); }
-int2048 operator - (int2048_view lhs, int2048 &&rhs) { return -std::move(rhs -= lhs); }
-int2048 operator - (int2048 &&lhs, int2048_view rhs) { return std::move(lhs -= rhs);  }
-int2048 operator - (int2048 &&lhs, int2048 &&rhs) { return std::move(lhs -= std::move(rhs)); }
+int2048 &operator -= (int2048 &lhs, int2048_view rhs) { return lhs += rhs.reverse(); }
+int2048 &operator -= (int2048 &lhs, int2048 &&rhs) { return lhs += std::move(rhs.reverse()); }
 
-int2048 operator * (int2048_view lhs, int2048 &&rhs) { return std::move(rhs *= lhs); }
-int2048 operator * (int2048 &&lhs, int2048_view rhs) { return std::move(lhs *= rhs); }
-int2048 operator * (int2048 &&lhs, int2048 &&rhs) { return std::move(lhs *= std::move(rhs)); }
+int2048 operator - (int2048_view lhs, int2048_view rhs) { return lhs + rhs.reverse(); }
+int2048 operator - (int2048_view lhs, int2048 &&rhs) { return std::move(rhs.reverse() += lhs); }
+int2048 operator - (int2048 &&lhs, int2048_view rhs) { return std::move(lhs += rhs.reverse());  }
+int2048 operator - (int2048 &&lhs, int2048 &&rhs) { return std::move(lhs += std::move(rhs.reverse())); }
+
+// int2048 operator * (int2048_view lhs, int2048 &&rhs) { return std::move(rhs *= lhs); }
+// int2048 operator * (int2048 &&lhs, int2048_view rhs) { return std::move(lhs *= rhs); }
+// int2048 operator * (int2048 &&lhs, int2048 &&rhs) { return std::move(lhs *= std::move(rhs)); }
 
 } // namespace dark
